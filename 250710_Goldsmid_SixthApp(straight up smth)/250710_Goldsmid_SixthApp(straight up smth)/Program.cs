c039e28e66b6b4e92f722353ea8530339f7d0d62
prostekuba1303 <jakubgoldsmid@gmail.com>
2025-07-10 15:59:47 +0200

    initial commit

---
+++ 250710_Goldsmid_SixthApp(straight up smth)/250710_Goldsmid_SixthApp(straight up smth)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+
+namespace _250710_Goldsmid_SixthApp_straight_up_smth_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int increment1 = 5;
+            int increment2 = 10;
+
+            int doubleincrement1 = 3;
+            int doubleincrement2 = 6;
+
+            while (true)
+            {
+                int number1 = rnd.Next(increment1, increment2);
+                char operator1 = '+';
+                int number2 = rnd.Next(increment1, increment2);
+                Console.WriteLine($"what is {number1} {operator1} {number2}");
+                Console.ReadLine();
+
+                increment1 = increment1 + doubleincrement1;
+                increment2 = increment2 + doubleincrement2;
+
+                doubleincrement1 = doubleincrement1 + 5;
+                doubleincrement2 = doubleincrement2 + 10;
+            }
+
+        }
+    }
+}
